fb102269fbc232a96b2596066cd4b2d8f474bc61
Arzugar <lulu975@ouvaton.org>
2023-05-15 20:01:45 +0200

    la cest mieux normalement

--- file_calculate_descriptors.py
+++ file_calculate_descriptors.py
@@ -7,8 +7,10 @@ from tqdm import tqdm
 DEFAULT_N_FEATURES = 256
 
 
-def compute_descriptor_img(infile, outfile = '', nfeatures = DEFAULT_N_FEATURES, safe_path = False) :
-    if not safe_path :
+def compute_descriptor_img(
+    infile, outfile="", nfeatures=DEFAULT_N_FEATURES, safe_path=False
+):
+    if not safe_path:
         infile = os.path.abspath(infile)
         outfile = os.path.abspath(outfile)
 
@@ -18,64 +20,75 @@ def compute_descriptor_img(infile, outfile = '', nfeatures = DEFAULT_N_FEATURES,
 
     sift = cv.SIFT_create(nfeatures=nfeatures)
 
-    _, des = sift.detectAndCompute(grayscale,None)
-    if outfile != '' : 
-    # format de sortie : n : nbr de descripteur : 4 bytes, suivit de 128 * n flottants, chacun sur 4 bytes
-        with open(outfile, "wb") as outfile :
-
+    _, des = sift.detectAndCompute(grayscale, None)
+    if outfile != "":
+        # format de sortie : n : nbr de descripteur : 4 bytes, suivit de 128 * n flottants, chacun sur 4 bytes
+        with open(outfile, "wb") as outfile:
             outfile.write(struct.pack("<l", len(des)))
-            
-            for d in des :
-                for f in d :
-                    outfile.write( struct.pack("<f", f)) # c'est très probablement des fottants 32 bits donc f est ok 
-    else :
+
+            for d in des:
+                for f in d:
+                    outfile.write(
+                        struct.pack("<f", f)
+                    )  # c'est très probablement des fottants 32 bits donc f est ok
+    else:
         return des
 
-def compute_descriptor_dir(indir, outdir, nfeatures = DEFAULT_N_FEATURES, safe_path = False, verbose = False):
-    if not safe_path : 
+
+def compute_descriptor_dir(
+    indir, outdir, nfeatures=DEFAULT_N_FEATURES, safe_path=False, verbose=False
+):
+    if not safe_path:
         indir = os.path.abspath(indir)
         outdir = os.path.abspath(outdir)
-    files = [indir+'/'+f for f in os.listdir(indir) if os.path.isfile(indir+'/'+f)]
-    if verbose :
+    files = [
+        indir + "/" + f for f in os.listdir(indir) if os.path.isfile(indir + "/" + f)
+    ]
+    if verbose:
         it = tqdm(files)
-    else :
+    else:
         it = files
-    for f in it :
-        try :
-            if not str.endswith(f, ".jpg") :
+    for f in it:
+        try:
+            if not str.endswith(f, ".jpg"):
                 continue
-            filename = f.split('/')[-1].split('.')[0]
-            compute_descriptor_img(f, outfile=outdir+'/'+'_descr'+filename, nfeatures=nfeatures, safe_path= True)
-        except Exception :
-            print(f"Issue occured on file : {filename}" )
-    
-if __name__ == '__main__' :
+            filename = f.split("/")[-1].split(".")[0]
+            compute_descriptor_img(
+                f,
+                outfile=outdir + "/" + "_descr" + filename,
+                nfeatures=nfeatures,
+                safe_path=True,
+            )
+        except Exception:
+            print(f"Issue occured on file : {filename}")
+
+
+if __name__ == "__main__":
     # Parsing à la main des entrées, éventuellement utiliser un module pour le faire proprement
     args = sys.argv
 
-    if len(args) >= 2 :
+    if len(args) >= 2:
         entree = args[1]
-        entree = os.path.abspath (entree)
+        entree = os.path.abspath(entree)
         entree_type = os.path.isfile(entree)
-    else :
+    else:
         print("No input provided")
         exit(1)
-    if len(args) == 3 :
+    if len(args) == 3:
         sortie = args[2]
-        sortie = os.path.abspath (sortie)
+        sortie = os.path.abspath(sortie)
         sortie_type = os.path.isfile(sortie)
-        assert(entree_type == sortie_type)
-    else :
-        if entree_type :
-            filename = entree.split('/')[-1].split('.')[0]
-            parent_dir = '/'.join(entree.split('/')[:-1])
-            sortie = parent_dir+"/_descr"+filename
-        else :
+        assert entree_type == sortie_type
+    else:
+        if entree_type:
+            filename = entree.split("/")[-1].split(".")[0]
+            parent_dir = "/".join(entree.split("/")[:-1])
+            sortie = parent_dir + "/_descr" + filename
+        else:
             print("No output directory provided")
             exit(1)
 
-
-    if entree_type : 
+    if entree_type:
         compute_descriptor_img(entree, sortie, safe_path=True)
-    else :
-        compute_descriptor_dir(entree,sortie, safe_path=True, verbose=True)
+    else:
+        compute_descriptor_dir(entree, sortie, safe_path=True, verbose=True)
