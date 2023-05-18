bb44ad788dcd74cb636dcf20f425583f91d3cf7f
Arzugar <lulu975@ouvaton.org>
2023-05-18 08:33:13 +0200

    code allégé, marche pas encore

--- utils.py
+++ utils.py
@@ -17,9 +17,9 @@ DEFAULT_N_FEATURES = 256
 
 
 class Image:
-    def __init__(self, path: str, id : None =None , name : str | None =None, descr_path :str | None =None) -> None:
+    def __init__(self, path: str, id : None |int =None , name : str | None =None, descr_path :str | None =None) -> None:
         self.path = os.path.abspath(path)
-        self.name = name
+        self.name = self.path.split("/")[-1]
         self.id = id
         self.descr = None
         self.nb_descr = None
@@ -46,7 +46,7 @@ class Image:
                         i+=1
     
     
-    def compute_descr(self,
+    def compute_descr(self, save = False,
         outfile="", nfeatures=DEFAULT_N_FEATURES
     ):
         outfile = os.path.abspath(outfile)
@@ -63,7 +63,7 @@ class Image:
         self.descr = np.array(des)
         self.descr.reshape((self.size,128))
 
-        if outfile != "":
+        if save:
             # format de sortie : n : nbr de descripteur : 4 bytes, suivit de 128 * n flottants, chacun sur 4 bytes
             with open(outfile, "wb") as outfile:
                 outfile.write(struct.pack("<l", len(des)))
@@ -77,12 +77,14 @@ class Image:
 
 
 class Database:
-    def __init__(self, dir_path : str = "", auto_init = True, nb_descr_per_img=DEFAULT_N_FEATURES) -> None:
+    def __init__(self, dir_path : str = "", auto_init = True, verbose = False, nb_descr_per_img=DEFAULT_N_FEATURES) -> None:
         self.dir_path = os.path.abspath(dir_path)
         self.images = None
         self.nb_descr_per_img = nb_descr_per_img
         self.name = dir_path.split('/')[-1]
         self.descr_path = self.dir_path + "../" + f"_descr_{self.nb_descr_per_img}" + self.name
+        if auto_init : 
+            self.auto_init(verbose=verbose)
 
     def of_images(self, arr):
         pass
@@ -95,17 +97,12 @@ class Database:
             if os.path.isfile(self.dir_path + "/" + f)
         ]
         self.images = np.empty((len(files_paths), self.nb_descr_per_img, 128), dtype=Image)
-        files_names = [
-            f
-            for f in os.listdir(self.dir_path)
-            if os.path.isfile(self.dir_path + "/" + f)
-        ]
+
         descr_dir_exist = os.path.isdir(self.descr_path)
         for i,f_path in enumerate(files_paths):
             self.images[i] = Image(f_path)
-            self.images[i].name = files_names[i]
             if descr_dir_exist :
-                self.images[i].descr_path = self.descr_path + '/' + files_names[i]
+                self.images[i].descr_path = self.descr_path + '/' + self.images[i].name
     
             
             
@@ -129,13 +126,13 @@ class Database:
         
         os.mkdir(self.descr_path)
         for im in it :
-            outfile = ""
-            if save : 
-                outfile = self.descr_path + '/' + im.name
-            im.compute_descr(outfile = outfile, nfeatures = self.nb_descr_per_img)
+            outfile = self.descr_path + '/' + im.name
+                
+            im.compute_descr(outfile = outfile, save = save, nfeatures = self.nb_descr_per_img)
 
 
     def auto_init(self, verbose = False) :
+        print("Initialisation ...")
         self.load_images()
         if os.path.isdir(self.descr_path) :
             self.load_descriptors(verbose=verbose)
@@ -143,36 +140,17 @@ class Database:
             self.compute_descr(save=True, verbose=verbose)
 
 
-""" def compute_descriptor_dir(
-    indir, outdir, nfeatures=DEFAULT_N_FEATURES, safe_path=False, verbose=False
-):
-    if not safe_path:
-        indir = os.path.abspath(indir)
-        outdir = os.path.abspath(outdir)
-    files = [
-        indir + "/" + f for f in os.listdir(indir) if os.path.isfile(indir + "/" + f)
-    ]
-    if verbose:
-        it = tqdm(files)
-    else:
-        it = files
-    for f in it:
-        try:
-            if not str.endswith(f, ".jpg"):
-                continue
-            filename = f.split("/")[-1].split(".")[0]
-            compute_descriptor_img(
-                f,
-                outfile=outdir + "/" + "_descr" + filename,
-                nfeatures=nfeatures,
-                safe_path=True,
-            )
-        except Exception:
-            print(f"Issue occured on file : {filename}")
 
- """
-""" if __name__ == "__main__":
-    # Parsing à la main des entrées, éventuellement utiliser un module pour le faire proprement
+def test01():
+    p = "image_data/very_small/100000.jpg"
+    im = Image(p)
+    im.compute_descr()
+    print("ok")
+    exit(0)
+
+if __name__ == "__main__":
+    test01()
+
     args = sys.argv
 
     if len(args) >= 2:
@@ -182,22 +160,9 @@ class Database:
     else:
         print("No input provided")
         exit(1)
-    if len(args) == 3:
-        sortie = args[2]
-        sortie = os.path.abspath(sortie)
-        sortie_type = os.path.isfile(sortie)
-        assert entree_type == sortie_type
-    else:
-        if entree_type:
-            filename = entree.split("/")[-1].split(".")[0]
-            parent_dir = "/".join(entree.split("/")[:-1])
-            sortie = parent_dir + "/_descr" + filename
-        else:
-            print("No output directory provided")
-            exit(1)
-
-    if entree_type:
-        compute_descriptor_img(entree, sortie, safe_path=True)
-    else:
-        compute_descriptor_dir(entree, sortie, safe_path=True, verbose=True)
- """
+    if entree_type :
+        im = Image(entree)
+        im.compute_descr()
+    else :
+        d = Database(entree, auto_init=True, verbose= True)
+        print("C'est fait")
