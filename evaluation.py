a204cbd67e8d70c724b0f569bb5d9f106d8a23a5
Arzugar <lulu975@ouvaton.org>
2023-05-22 23:35:55 +0200

    ajout du fichier evaluation, reste très primitif

---
+++ evaluation.py
@@ -0,0 +1,26 @@
+from utils import *
+from query_methods import * 
+
+
+
+if __name__ == "__main__" : 
+    args = sys.argv
+
+    assert len(args) == 3
+    datapath = args[1]
+    sample_size = int(args[2])
+
+    d = Database(datapath, auto_init=True, verbose= True)
+
+    total_good = 0
+
+    search_f = kd_tree_search_func_gen(d, verbose=True)
+    for _ in tqdm(range(sample_size)):
+        query_im = rd.choice(d.images)
+
+        result = query(d, query_im, search_f,im_k=5, descr_k=20, verbose=True, weight= lambda x:1/(x + 0.001))
+        
+        has_found_good_one = any([x[0].group_id == query_im.group_id for x in result])
+        if has_found_good_one :
+            total_good +=1
+    print("Moyenne de retour : ",total_good/sample_size)
