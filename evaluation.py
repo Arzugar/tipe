cfb0eed9acdd2b952b18314b06406f4f2f55b57d
Arzugar <lulu975@ouvaton.org>
2023-05-24 23:34:12 +0200

    modif des bornes d'étude

--- evaluation.py
+++ evaluation.py
@@ -21,10 +21,11 @@ if __name__ == "__main__":
         datapath, auto_init=True, verbose=False, nb_descr_per_img=nb_descriptors
     )
     print("Taille du nuage de points : ", d.taille_nuage())
-
+    print("Nombre de descr par image : ", nb_descriptors)
+    print("Sample size : ", sample_size)
     descr_k_vals = range(10, 50, 10)
     nb_tables_val = range(2, 20, 2)
-    nb_fun_per_table_val = range(1, 10, 1)
+    nb_fun_per_table_val = range(1, 5, 1)
     r_val = range(5, 20, 5)
 
     for nb_tables, nb_fun_per_table, r in itertools.product(
@@ -33,7 +34,7 @@ if __name__ == "__main__":
         start_time = time.time()
 
         # search_f = kd_tree_search_func_gen(d, verbose=True)
-        search_f = init_lsh(
+        lsh_tables, search_f = init_lsh(
             d,
             verbose=False,
             r=r,
@@ -43,11 +44,13 @@ if __name__ == "__main__":
 
         build_time = time.time() - start_time
 
+        avg_bucket_per_tables = lsh_tables.nb_buck_per_table()
+
         for descr_k in descr_k_vals:
             total_score = 0
             total_query_time = 0
 
-            for _ in tqdm(range(sample_size)):
+            for _ in range(sample_size):
                 query_im = rd.choice(d.images)
                 # détermine l'image associée
 
@@ -72,8 +75,8 @@ if __name__ == "__main__":
                 total_score += score
 
             print(
-                f"Param : descr_k : {descr_k} | nb_tables : {nb_tables} | nb_fun_per_table : {nb_fun_per_table} | r : {r}\n"
+                f"Param : descr_k : {descr_k} | nb_tables : {nb_tables} | nb_fun_per_table : {nb_fun_per_table} | r : {r}"
             )
             print(
-                f"Result: build_time : {build_time} | avg_score : {total_score/sample_size} | avg_query_time : {total_query_time/sample_size}\n"
+                f"Result: build_time : {build_time} | avg_bucket_per_table : {avg_bucket_per_tables} | avg_score : {total_score/sample_size} | avg_query_time : {total_query_time/sample_size}\n"
             )
