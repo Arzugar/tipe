654b1f5ff857c052a8ac96391af6ec51cd0cd5d6
Arzugar <lulu975@ouvaton.org>
2023-05-26 16:22:22 +0200

    Mise en place des queries LSH avec falconn

---
+++ query_with_falconn.py
@@ -0,0 +1,42 @@
+import falconn as fa
+import sys
+from utils import *
+import random as rd
+
+number_of_tables = 30
+
+falconn_default_index_params = fa.LSHConstructionParameters()
+falconn_default_index_params.dimension = 128
+falconn_default_index_params.lsh_family = fa.LSHFamily.Hyperplane
+falconn_default_index_params.distance_function = fa.DistanceFunction.EuclideanSquared
+falconn_default_index_params.l = number_of_tables
+# for sparse data set it to 2 , for dense data : 1
+falconn_default_index_params.num_rotations = 2
+falconn_default_index_params.seed = 5721840
+# we want to use all the available threads to set up
+falconn_default_index_params.num_setup_threads = 0
+falconn_default_index_params.storage_hash_table = (
+    fa.StorageHashTable.BitPackedFlatHashTable
+)
+# we build 18-bit hashes so that each table has
+# 2^18 bins; this is a good choise since 2^18 is of the same
+# order of magnitude as the number of data points
+fa.compute_number_of_hash_functions(18, falconn_default_index_params)
+
+
+def init_index(d: Database, params=falconn_default_index_params):
+    print(params)
+    index = fa.LSHIndex(params)
+
+    index.setup(d.to_array())
+
+    return index
+
+
+def falconn_query_image(index, query_im, k, nb_probes: int = 1):
+    index.set_num_probes(nb_probes)
+    distances, descripeurs = index.find_k_nearest_neighbors_batch(query_im.descr, k)
+    return distances, descripeurs
+
+
+# a = query_object.get_candidates_with_duplicates(query_im.descr[0])
