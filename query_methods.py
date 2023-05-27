32408756e384be1be3674c288d8781a746f3a200
Arzugar <lulu975@ouvaton.org>
2023-05-27 10:28:29 +0200

    Mise a jour pour supporter le nouveau format de query

--- query_methods.py
+++ query_methods.py
@@ -2,33 +2,35 @@
 
 from utils import *
 import numpy.linalg as la
-import heapq as hp
-import collections as cl
-from scipy.spatial import KDTree
-from lsh import *
 
 import numpy.random as rd
+from typing import List, Tuple
 
 
-def second_closest_ratio(h, max_ratio):
-    if len(h) == 0:
-        return False
-    d1, first_im = h[0]
-    for d2, snd_im in h:
-        if first_im.group_id != snd_im.group_id:
+def second_closest_ratio_test(
+    d: Database, dists: List[np.float32], closests, max_ratio
+):
+    if len(closests) == 0:
+        return True  # à voir si c'est le plus intelligent
+    d1, first_im = dists[0], closests[0]
+    for d2, snd_descr in zip(dists[1:], closests[1:]):  # type: ignore
+        if first_im.group_id != d.reverse_descr_index(snd_descr).group_id:
             return d1 <= max_ratio * d2
     return False
 
 
+# Modifier pour appeler une fonction qui renvoie tous les descripteurs proches de ceux de l'image
 # Fonction générique pour
 def query(
     data: Database,
     query_im: Image,
     search_func,
+    index,
+    specific_params={},
     im_k: int = 5,
     descr_k: int = 20,
     verbose=False,
-    weight=lambda x: 1,
+    weight=lambda x: 1 / (x + 0.00001),
     snd_closest_ratio=True,
     max_ratio: float = 0.75,
     ignore_self=True,
@@ -45,58 +47,34 @@ def query(
     """
     histogram = dict()
     if verbose:
-        it = tqdm(query_im.descr, desc="Calcul des plus proches voisins")
-    else:
-        it = query_im.descr
-    for query_descr in it:
-        h = search_func(data, query_descr, descr_k)
-        if ignore_self:
-            h = list(filter(lambda x: x[1].id != query_im.id, h))
-
+        print("Searching for voisins")
+    result: Tuple[List[List[np.float32]],] = search_func(
+        index, query_im, descr_k, specific_params
+    )
+    if verbose:
+        print("Voisins trouvés\nVote en cours")
+    for dists, k_closests_descr in result:
         # skip this descriptor if not relevant enought
-        if snd_closest_ratio and not second_closest_ratio(h, max_ratio):
+        if snd_closest_ratio and not second_closest_ratio_test(
+            data, dists, k_closests_descr, max_ratio
+        ):
             continue
 
-        for dist, im in h:
+        for dist, descr in zip(dists, k_closests_descr):
+            associated_im = d.reverse_descr_index(descr)
+            if ignore_self and associated_im.id == query_im.group_id:
+                continue
+
             # incrémente si existe déjà, sinon met à 1 * weight
             if vote_for_class:
-                histogram[im.group_id] = histogram.get(im, 0) + weight(dist)
+                histogram[im.group_id] = histogram.get(
+                    associated_im.group_id, 0
+                ) + weight(dist)
             else:
-                histogram[im] = histogram.get(im, 0) + weight(dist)
-
+                histogram[im] = histogram.get(associated_im, 0) + weight(dist)
+    if verbose:
+        print("Vote terminé")
     return sorted(histogram.items(), key=lambda x: x[1], reverse=True)[:im_k]
-
-
-def basic_search(data: Database, query_descr, descr_k: int):
-    return basic_search_base(data.iter_descr(), query_point=query_descr, k=descr_k)
-
-
-def query_on_tree(data: Database, tree, descr_k, query_descr):
-    d, inds = tree.query(query_descr, k=descr_k, p=2)
-    inds = map(lambda x: data.image_of_descr_index(x), inds)
-    return zip(d, inds)
-
-
-def kd_tree_search_func_gen(data: Database, verbose=False):
-    print("Building tree...")
-    d = data.to_array()
-    tree = KDTree(d, leafsize=10, balanced_tree=True)
-    print("Tree built succesfully !")
-    return lambda _, query_descr, descr_k: query_on_tree(
-        data, tree, descr_k, query_descr
-    )
-
-
-def init_lsh(
-    data: Database, verbose=False, nb_tables=10, nb_fun_per_table=2, r=10
-):  # augmenter le nbr de fonctions par table fait très vite augmenter le nombre de buckets
-    s = Lsh(nb_fun_per_table=nb_fun_per_table, nb_tables=nb_tables, r=r)
-    if verbose:
-        print("Preprosessing ...")
-    s.preprocess(data, verbose=verbose)
-    if verbose:
-        print("Preprossesing finished !")
-    return s, lambda _, query_descr, descr_k: s.query_knn(descr_k, query_descr)
 
 
 if __name__ == "__main__":
@@ -107,46 +85,7 @@ if __name__ == "__main__":
     # impath = args[2]
     d = Database(datapath, auto_init=True, verbose=True)
 
-    print("Nombre de points du nuage : ", d.taille_nuage())
+    print("Nombre de points du nuage : ", d.taille_nuage)
 
     query_im = rd.choice(d.images)
     print("Classe de l'image recherchée : ", query_im.group_id)
-
-    # search_f = kd_tree_search_func_gen(d, verbose=True)
-    # search_f = basic_search
-
-    l, search_f = init_lsh(
-        d,
-        verbose=True,
-    )
-    print(l.nb_buck_per_table())
-
-    result = query(
-        d,
-        query_im,
-        search_f,
-        im_k=1,
-        descr_k=50,  # bug : si le nombre de tables est >= descr_k , aucun voisins ne sont trouvés
-        verbose=True,
-        weight=lambda x: 1 / (x + 0.001),
-        snd_closest_ratio=False,
-        ignore_self=True,
-    )
-    print(result[0][0].group_id)
-    search_f = kd_tree_search_func_gen(d, verbose=True)
-
-    # search_f = basic_search
-    # search_f = init_lsh(d, verbose=True)
-    result = query(
-        d,
-        query_im,
-        search_f,
-        im_k=5,
-        descr_k=20,
-        verbose=True,
-        weight=lambda x: 1 / (x + 0.001),
-        snd_closest_ratio=True,
-    )
-
-    for r in result:
-        print(r[0].name, r[1])
