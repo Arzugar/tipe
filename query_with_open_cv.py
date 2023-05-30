00c45bcc0fc461e8c9cc503f44aa9a93066829a2
Arzugar <lulu975@ouvaton.org>
2023-05-30 13:54:50 +0200

    changé le test, il semble que seul le premier vecteur soit indexé

--- query_with_open_cv.py
+++ query_with_open_cv.py
@@ -1,3 +1,6 @@
+#!/bin/python3
+
+
 import cv2 as cv
 from utils import *
 import random as rd
@@ -23,8 +26,7 @@ def ocv_init_index(
     # m.add(t)
     for im in d.images:
         m.add([im.descr])
-
-    m.train()
+        m.train()
 
     return m
 
@@ -35,27 +37,14 @@ def ocv_query_search(index: cv.FlannBasedMatcher, query_im, k, specific_params=N
     return [[x.distance for x in m] for m in r], [[x.trainIdx for x in m] for m in r]
 
 
-""" 
 if __name__ == "__main__":
-    args = sys.argv
+    dpath = "./image_data/very_small"
+    d = Database(dpath, nb_descr_per_img=10)
+    index = ocv_init_index(d)
+    query_im = d.images[2]
+    print(query_im.name)
+    des = query_im.descr
 
-    assert len(args) == 2
-    datapath = args[1]
-    d = Database(datapath, auto_init=True, verbose=True)
-
-    print("Nombre de points du nuage : ", d.taille_nuage())
-
-    query_im = rd.choice(d.images)
-    print("Classe de l'image recherchée : ", query_im.group_id)
-
-    # index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-    index_params = dict(
-        algorithm=FLANN_INDEX_LSH,
-        trees=5,
-        table_number=6,  # 12
-        key_size=12,  # 20
-        multi_probe_level=1,
-    )
-    m = init_matcher(d, index_param=index_params)
-    m.train()
- """
+    r = ocv_query_search(index, query_im, 10, None)
+    print(r[0][0])
+    print(r[1][0])
