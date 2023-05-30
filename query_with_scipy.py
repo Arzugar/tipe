9365c193025f5f0c92592041584c3795d2a0391d
Arzugar <lulu975@ouvaton.org>
2023-05-30 17:33:04 +0200

    Ajout de la possibilité de chercher par descripteur

--- query_with_scipy.py
+++ query_with_scipy.py
@@ -13,10 +13,19 @@ def scipy_init_index(d: Database, params=None):
 def scipy_query_image(index, query_im, k, specific_params=None):
     distances, neighbors = [[]] * query_im.nb_descr, [[]] * query_im.nb_descr
     for i, d in enumerate(query_im.descr):
-        dists, indices = index.query(d, k)
+        dists, indices = index.query(d, k, workers=-1, p=2)
         distances[i] = dists
         neighbors[i] = indices
+
     return distances, neighbors
+
+
+def scipy_query_descr(index, descr, k, specific_params=None):
+    distances, nei = index.query(descr, k, workers=-1, p=2)
+    sorted_data = sorted(zip(distances, nei), key=lambda x: x[0])  # type: ignore
+    sorted_distances, sorted_points = zip(*sorted_data)
+
+    return sorted_distances, sorted_points
 
 
 if __name__ == "__main__":
